a6fbba5ba6953dc9b9715dda901c1bb58b2bbb0a
Shikhar Mishra <mshikhar353@gmail.com>
2026-01-06 13:34:36 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector(".nav3 .ri-menu-3-line");
+const navMenu = document.querySelector(".nav2");
+
+menuIcon.addEventListener("click", () => {
+navMenu.classList.toggle("show");
+});
